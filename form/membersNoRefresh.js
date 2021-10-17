e78dfeb0bfdef9b8d4e9c50b51b9ad1e4ae70a12
imLucyk <88808248+imLucyk@users.noreply.github.com>
2021-10-17 14:37:44 +0900

    [#10]새로고침 없이 회원 CRUD read

---
+++ form/membersNoRefresh.js
@@ -0,0 +1,72 @@
+const queryString = new URLSearchParams(window.location.search);
+const nameText = queryString.get('input-text');
+
+// const inputTextObjects = document.getElementsByName('input-text');
+// const inputTextObject = inputTextObjects[0];
+
+const inputTextObject = document.getElementsByName('input-text')[0];
+inputTextObject.value = nameText;
+
+const inputHiddens = queryString.getAll('input-hidden');
+const inputHidden = inputHiddens[0];
+
+inputTextObject.focus();
+// inputTextObject.blur();
+
+const membersGet = sessionStorage.getItem('members');
+const membersLogical = membersGet || '[]';
+const members = JSON.parse(membersLogical);
+
+
+const membersSubmit = function(f) {
+  const inputTextObject = f['input-text'];
+  try {
+    const evalReturn = eval(inputTextObject.value);
+    console.log(evalReturn);
+  } catch(error) {
+    console.error(error);
+    alert(error);
+    return false;
+  }
+}
+const membersSet = function() {
+  const membersSet = JSON.stringify(members);
+  sessionStorage.setItem('members', membersSet);
+}
+
+const membersCreate = function(member) {
+  members.push(member);
+  membersSet();
+  return members;
+};
+
+const membersRead = function() {
+  const t = document.getElementById('name');
+  t.innerHTML = '';
+  for (let index in members) {
+    t.innerHTML += '<input type="text" name="members-name" value="' + members[index] + '">';
+    t.innerHTML += '<button onclick="membersDelete(' + index + ')">Delete</button>';
+    t.innerHTML += '\n';
+  }
+  console.log('Readed', members);
+  return members;
+}
+
+const membersDelete = function(index) {
+  members.splice(index, 1)
+  membersSet();
+  return members;
+}
+
+const membersUpdate = function(index, member){
+  members[index] = member;
+  membersSet();
+  return members;
+
+}
+
+membersRead();
+
+const testFunction = function() {
+  return undefined;
+};
