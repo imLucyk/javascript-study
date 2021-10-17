8918065b8290dc8a091b7cef36a850f65b3806d1
imLucyk <88808248+imLucyk@users.noreply.github.com>
2021-10-17 15:19:19 +0900

    [#10]새로고침 없이 회원 CRUD create

--- form/membersNoRefresh.js
+++ form/membersNoRefresh.js
@@ -17,27 +17,16 @@ const membersGet = sessionStorage.getItem('members');
 const membersLogical = membersGet || '[]';
 const members = JSON.parse(membersLogical);
 
-
-const membersSubmit = function(f) {
-  const inputTextObject = f['input-text'];
-  try {
-    const evalReturn = eval(inputTextObject.value);
-    console.log(evalReturn);
-  } catch(error) {
-    console.error(error);
-    alert(error);
-    return false;
-  }
-}
 const membersSet = function() {
   const membersSet = JSON.stringify(members);
   sessionStorage.setItem('members', membersSet);
 }
 
-const membersCreate = function(member) {
-  members.push(member);
+const membersCreate = function(form) {
+  const inputTextObject = form['input-text'];
+  members.push(inputTextObject.value);
   membersSet();
-  return members;
+  return membersRead();
 };
 
 const membersRead = function() {
