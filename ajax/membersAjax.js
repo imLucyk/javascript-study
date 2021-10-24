7ceca323934a3dc3efaff23695945e0e2b2e4ca0
imLucyk <88808248+imLucyk@users.noreply.github.com>
2021-10-24 18:51:26 +0900

    [#12] Ajax Backend 통신 create

---
+++ ajax/membersAjax.js
@@ -0,0 +1,101 @@
+const queryString = new URLSearchParams(window.location.search);
+const nameText = queryString.get('input-text');
+
+// const inputTextObjects = document.getElementsByName('input-text');
+// const inputTextObject = inputTextObjects[0];
+
+// const inputTextObject = document.getElementsByName('input-text')[0];
+// inputTextObject.value = nameText;
+
+// const inputHiddens = queryString.getAll('input-hidden');
+// const inputHidden = inputHiddens[0];
+
+// inputTextObject.focus();
+// inputTextObject.blur();
+
+const membersGet = sessionStorage.getItem('members');
+const membersLogical = membersGet || '[]';
+const members = JSON.parse(membersLogical);
+
+const membersSet = function() {
+  const membersSet = JSON.stringify(members);
+  sessionStorage.setItem('members', membersSet);
+}
+
+const membersCreate = function(form) {
+  const memberNameObject = form['member-name'];
+  const memberAgeObject = form['member-age'];
+  const member = {
+    name: memberNameObject.value,
+    age: memberAgeObject.value
+  };
+  const successFunction = function() {
+    memberNameObject.value = '';
+    memberAgeObject.value = '';
+    membersRead();
+  }
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function() {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      successFunction();
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open('POST', 'http://localhost:3100/api/v1/members');
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send(JSON.stringify(member));
+};
+
+
+const membersRead = function() {
+  const tagDivParent = document.getElementById('tag-div-parent');
+  tagDivParent.innerHTML = '';
+  const tagDivChild = document.getElementById('tag-div-child');
+  for (let index in members) {
+    const newDivChild = tagDivChild.cloneNode(true);
+    tagDivParent.appendChild(newDivChild);
+    const membersNameObject = document.getElementsByName('members-name')[index];
+    const membersAgeObject = document.getElementsByName('members-age')[index];
+    const membersUpdateObject = document.getElementsByName('members-update')[index];
+    const membersDeleteObject = document.getElementsByName('members-delete')[index];
+    // membersNameObject.value = members[index];
+    membersNameObject.value = members[index].name;
+    membersAgeObject.value = members[index].age;
+    membersUpdateObject.index = index;
+    membersDeleteObject.index = index;
+  }
+  console.log('Readed', members);
+  return members;
+};
+
+
+const membersDelete = function(index) {
+  members.splice(index, 1)
+  membersSet();
+  return membersRead();
+}
+
+const membersUpdate = function(index) {
+  const name = document.getElementsByName('members-name')[index].value;
+  const age = document.getElementsByName('members-age')[index].value;
+  members[index] = {
+    name: name,
+    age: age
+  }
+  membersSet();
+  return membersRead();
+};
+
+
+membersRead();
+
+const testFunction = function() {
+  return undefined;
+};
