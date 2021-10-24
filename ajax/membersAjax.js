4565101c83ff78f96bbc316fc8d688c37fca42ad
imLucyk <88808248+imLucyk@users.noreply.github.com>
2021-10-24 19:17:58 +0900

    [#12] Ajax Backend 통신 delete

--- ajax/membersAjax.js
+++ ajax/membersAjax.js
@@ -92,10 +92,26 @@ const membersRead = function() {
 };
 
 const membersDelete = function(index) {
-  members.splice(index, 1)
-  membersSet();
-  return membersRead();
-}
+  const url = 'http://localhost:3100/api/v1/members/' + index;
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function () {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      membersRead();
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open('DELETE', url);
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send();
+};
+
 
 const membersUpdate = function(index) {
   const name = document.getElementsByName('members-name')[index].value;
